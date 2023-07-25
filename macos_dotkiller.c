9c0033a529d1ad468f5ce3fa04bbb3595e3014cb
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2023-07-25 23:39:56 +0200

    Update macos_dotkiller.c

--- macos_dotkiller.c
+++ macos_dotkiller.c
@@ -2,134 +2,176 @@
 #include <furi_hal.h>
 
 #include <gui/gui.h>
+#include <gui/canvas.h>
 #include <gui/elements.h>
 #include <input/input.h>
 #include <dialogs/dialogs.h>
-
 #include <storage/storage.h>
+
 #include <stream/stream.h>
 #include <stream/buffered_file_stream.h>
 #include <toolbox/stream/file_stream.h>
 
 #include <stdlib.h>
 
-#define TAG "DotKiller"
+#include "macos_dotkiller_icons.h"
+#include "assets_icons.h"
 
-#define DOTFILE_VIEWER_APP_PATH_FOLDER "/ext"
+#define STORAGE_EXT_PATH_PREFIX "/ext"
 
-typedef struct {
-    int type;
-    InputEvent input;
-    bool confirmDelete;
-} DotKillContext;
-
-static void dotkill_input_callback(InputEvent* input_event, void* ctx) {
-    FuriMessageQueue* queue = ctx;
-    DotKillContext event = {
-        .type = input_event->type,
-        .input = *input_event,
-        .confirmDelete = false,
-    };
-    furi_message_queue_put(queue, &event, FuriWaitForever);
-}
-
-static void dotkill_render_callback(Canvas* canvas, void* ctx) {
-    DotKillContext* context = ctx;
-    
-    canvas_clear(canvas);
-    
-    canvas_draw_box(canvas, 0, 0, 127, 63);
-    canvas_set_color(canvas, ColorWhite);
-
-    canvas_draw_line(canvas, 0, 16, 127, 16);
-
-    canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str(canvas, 22, 30, "Delete all the useless");
-
-    canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str(canvas, 9, 40, ".dotfiles created by macOS");
-
-    canvas_draw_icon(canvas, 65, 52, &I_Ok_btn_9x9);
-
-    canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str(canvas, 77, 60, "Kill them all");
-
-    canvas_draw_icon(canvas, 4, 52, &I_Pin_back_arrow_10x8);
-
-    canvas_set_font(canvas, FontSecondary);
-    canvas_draw_str(canvas, 18, 60, "Exit");
-
-    canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str(canvas, 35, 12, "DOTKILLER");
-	
-	if (context->confirmDelete) {
-        canvas_set_font(canvas, FontPrimary);
-        canvas_draw_str_aligned(canvas, 32, 60, AlignCenter, AlignCenter, "Confirm delete all dotfiles?");
-        elements_button_left(canvas, "No!");
-        elements_button_right(canvas, "Yes!");
-    }
-}
-
-static void delete_dotfiles_recursively(const char* path) {
-    FileIterator* iterator = file_iterator_alloc(path, true);
-    if (iterator != NULL) {
-        while (file_iterator_next(iterator)) {
-            if (file_iterator_current_type(iterator) == FileTypeRegular) {
-                const char* filename = file_iterator_current_name(iterator);
-                char filepath[128];
-                snprintf(filepath, sizeof(filepath), "%s/%s", path, filename);
-
-                if (filename[0] == '._') {
-                    file_remove(filepath);
-                }
-            } else if (file_iterator_current_type(iterator) == FileTypeDirectory) {
-                const char* dirname = file_iterator_current_name(iterator);
-                if (dirname[0] != '._') {
-                    char dirpath[128];
-                    snprintf(dirpath, sizeof(dirpath), "%s/%s", path, dirname);
-                    delete_dotfiles_recursively(dirpath);
+// Definizione della funzione handle_input_event
+static void handle_input_event(InputKey key) {
+    switch (key) {
+        case InputKeyUp:
+        case InputKeyDown:
+        case InputKeyRight:
+        case InputKeyLeft:
+            // Gestione degli eventi per i tasti Up, Down, Right, Left
+            break;
+        case InputKeyOk:
+            if (show_confirm_dialog("Are you sure that you want to cancel all the useless hidden files created by macOS in your Flipper Zero?\nPress \"OK\" to confirm, or \"BACK\" to go back.") == DialogResultOk) {
+                StorageIterator* iterator = storage_iterator_alloc(STORAGE_EXT_PATH_PREFIX);
+                if (iterator) {
+                    while (storage_iterator_has_next(iterator)) {
+                        const char* file_name = storage_iterator_next(iterator);
+                        if (file_name[0] == '.' && file_name[1] == '_') {
+                            char file_path[128];
+                            snprintf(file_path, sizeof(file_path), "%s/%s", STORAGE_EXT_PATH_PREFIX, file_name);
+                            storage_common_remove(file_path);
+                        }
+                    }
+                    storage_iterator_free(iterator);
                 }
             }
-        }
-        file_iterator_free(iterator);
+            break;
+        case InputKeyBack:
+            processing = false;
+            break;
+        default:
+            break;
+    }
+}
+
+static void dotkill_input_callback(Event event, void* data) {
+    UNUSED(data);
+
+    if (event.type == EventTypeKey && event.input.type == InputTypePress) {
+        handle_input_event(event.input.key);
     }
 }
 
 
-int32_t flipper_dotkiller_app(void* p) {
-    UNUSED(p);
-    FuriMessageQueue* queue = furi_message_queue_alloc(8, sizeof(DotKillContext));
-    ViewPort* view_port = view_port_alloc();
-    view_port_input_callback_set(view_port, dotkill_input_callback, queue);
-    view_port_draw_callback_set(view_port, dotkill_render_callback, NULL);
+static void dotkill_render_callback(Canvas* canvas, void* data) {
+    UNUSED(data);
+
+    canvas_draw_frame(canvas, 0, 0, 128, 64);
+    canvas_draw_frame(canvas, 0, 0, 128, 64);
+	canvas_draw_box(canvas, 6, 6, 5, 14);
+	canvas_draw_box(canvas, 12, 6, 4, 2);
+	canvas_draw_box(canvas, 14, 8, 4, 2);
+	canvas_draw_box(canvas, 16, 10, 4, 6);
+	canvas_draw_box(canvas, 10, 18, 6, 2);
+	canvas_draw_box(canvas, 14, 16, 4, 2);
+	canvas_draw_box(canvas, 41, 5, 4, 13);
+	canvas_draw_box(canvas, 39, 7, 2, 4);
+	canvas_draw_box(canvas, 44, 7, 4, 4);
+	canvas_draw_box(canvas, 43, 17, 4, 3);
+	canvas_draw_dot(canvas, 42, 18);
+	canvas_draw_box(canvas, 50, 6, 4, 14);
+	canvas_draw_box(canvas, 52, 12, 6, 2);
+	canvas_draw_box(canvas, 56, 10, 6, 2);
+	canvas_draw_box(canvas, 58, 7, 4, 3);
+	canvas_draw_box(canvas, 53, 16, 9, 2);
+	canvas_draw_box(canvas, 58, 17, 4, 3);
+	canvas_draw_box(canvas, 53, 15, 6, 2);
+	canvas_draw_frame(canvas, 1, 1, 126, 62);
+	canvas_draw_box(canvas, 65, 18, 8, 2);
+	canvas_draw_box(canvas, 67, 9, 4, 9);
+	canvas_draw_box(canvas, 65, 8, 8, 2);
+	canvas_draw_box(canvas, 67, 4, 4, 3);
+	canvas_draw_box(canvas, 75, 5, 4, 13);
+	canvas_draw_box(canvas, 77, 18, 8, 2);
+	canvas_draw_box(canvas, 81, 16, 4, 2);
+	canvas_draw_box(canvas, 86, 5, 4, 13);
+	canvas_draw_box(canvas, 88, 18, 8, 2);
+	canvas_draw_box(canvas, 92, 16, 4, 2);
+	canvas_draw_circle(canvas, 29, 12, 7);
+	canvas_draw_circle(canvas, 29, 12, 6);
+	canvas_draw_circle(canvas, 29, 12, 5);
+	canvas_draw_dot(canvas, 25, 7);
+	canvas_draw_dot(canvas, 24, 8);
+	canvas_draw_dot(canvas, 24, 16);
+	canvas_draw_dot(canvas, 25, 17);
+	canvas_draw_dot(canvas, 33, 7);
+	canvas_draw_dot(canvas, 34, 8);
+	canvas_draw_dot(canvas, 34, 16);
+	canvas_draw_dot(canvas, 33, 17);
+	canvas_draw_circle(canvas, 29, 12, 4);
+	canvas_draw_dot(canvas, 25, 10);
+	canvas_draw_dot(canvas, 27, 8);
+	canvas_draw_dot(canvas, 31, 8);
+	canvas_draw_dot(canvas, 33, 10);
+	canvas_draw_dot(canvas, 25, 14);
+	canvas_draw_dot(canvas, 33, 14);
+	canvas_draw_dot(canvas, 31, 16);
+	canvas_draw_dot(canvas, 27, 16);
+	canvas_draw_dot(canvas, 13, 17);
+	canvas_draw_dot(canvas, 12, 17);
+	canvas_draw_dot(canvas, 13, 8);
+	canvas_draw_dot(canvas, 12, 8);
+	canvas_draw_line(canvas, 15, 10, 15, 17);
+	canvas_draw_dot(canvas, 11, 17);
+	canvas_draw_dot(canvas, 11, 7);
+	canvas_draw_dot(canvas, 11, 8);
+	canvas_draw_dot(canvas, 11, 6);
+	canvas_draw_box(canvas, 97, 5, 4, 15);
+	canvas_draw_box(canvas, 98, 5, 9, 2);
+	canvas_draw_box(canvas, 103, 6, 4, 3);
+	canvas_draw_box(canvas, 99, 12, 5, 2);
+	canvas_draw_box(canvas, 99, 18, 9, 2);
+	canvas_draw_box(canvas, 104, 17, 4, 1);
+	canvas_draw_box(canvas, 110, 6, 4, 14);
+	canvas_draw_box(canvas, 112, 10, 10, 2);
+	canvas_draw_box(canvas, 118, 6, 4, 4);
+	canvas_draw_box(canvas, 110, 6, 12, 2);
+	canvas_draw_line(canvas, 113, 12, 119, 18);
+	canvas_draw_line(canvas, 113, 13, 119, 19);
+	canvas_draw_line(canvas, 114, 12, 120, 18);
+	canvas_draw_box(canvas, 120, 16, 3, 4);
+	canvas_draw_icon(canvas, 57, 22, &I_Space_65x18)
+	canvas_draw_icon(canvas, 57, 42, &I_Space_65x18)
+	canvas_set_font(canvas, FontPrimary);
+	canvas_draw_str(canvas, 82, 34, "Clean")
+	canvas_set_font(canvas, FontPrimary);
+	canvas_draw_str(canvas, 87, 54, "Exit")
+	canvas_draw_icon(canvas, 74, 46, &I_Pin_back_arrow_10x8)
+	canvas_draw_icon(canvas, 70, 26, &I_Ok_btn_9x9)
+	canvas_draw_icon(canvas, 10, 20, &I_Target_40x40)
+	canvas_draw_icon(canvas, 10, 20, &I_Target_40x40)
+
+}
+
+int32_t flipper_dotkiller_app() {
     Gui* gui = furi_record_open(RECORD_GUI);
+    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+    
+    ViewPort* view_port = view_port_alloc();
+    view_port_draw_callback_set(view_port, dotkill_render_callback, view_port);
+    view_port_input_callback_set(view_port, dotkill_input_callback, event_queue);
+    
     gui_add_view_port(gui, view_port, GuiLayerFullscreen);
-    bool processing = true;
-    DotKillContext event;
-    bool deleteConfirmed = false;
-    do {
-        if (furi_message_queue_get(queue, &event, FuriWaitForever) == FuriStatusOk) {
-            if (event.input.type == InputTypeShort && event.input.key == InputKeyBack) {
-                processing = false;
-            } else if (event.input.type == InputTypeShort && event.input.key == InputKeyOk) {
-                event.confirmDelete = true;
-            } else if (event.input.type == InputTypeShort && event.input.key == InputKeyRight && event.confirmDelete) {
-                deleteConfirmed = true;
-                processing = false;
-            } else if (event.input.type == InputTypeShort && event.input.key == InputKeyLeft && event.confirmDelete) {
-                event.confirmDelete = false;
+    
+    while (processing) {
+        InputEvent event;
+        while (furi_message_queue_poll(event_queue, &event) == FuriStatusOk) {
+            if (event.type == EventTypeKey && event.input.type == InputTypePress) {
+                handle_input_event(event.input.key);
             }
         }
-    } while (processing);
-    view_port_enabled_set(view_port, false);
-    gui_remove_view_port(gui, view_port);
-    view_port_free(view_port);
-    furi_record_close(RECORD_GUI);
-
-    if (deleteConfirmed) {
-        // Delete dotfiles
-        delete_dotfiles_recursively(DOTFILE_VIEWER_APP_PATH_FOLDER);
     }
-
+    
+    gui_close(gui);
+    furi_message_queue_free(event_queue);
+    
     return 0;
 }
