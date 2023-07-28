bb0e73cb10159f23d8feebed770b0fad698d4a41
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2023-07-28 11:00:33 +0200

    Update confirm_dotkiller.c

--- confirm_dotkiller.c
+++ confirm_dotkiller.c
@@ -5,6 +5,8 @@
 #include <storage/storage.h>
 #include <gui/elements.h>
 #include <dialogs/dialogs.h>
+#include <stream/stream.h>
+#include <view/view_dispatcher.h>
 
 #include <stdlib.h>
 
@@ -17,18 +19,14 @@ static void confirm_input_callback(InputEvent* event, void* context) {
 
     if (event->type == EventTypeKey && event->input.type == InputTypePress) {
         switch (event->input.key) {
-	    case InputKeyUp:
-            case InputKeyDown:
-            case InputKeyOk:
-		break;
             case InputKeyLeft:
                 confirm_removal = false;
                 break;
 
             case InputKeyRight:
-    		confirm_removal = true;
-    		remove_hidden_files();
-    		break;
+    			confirm_removal = true;
+    			remove_hidden_files();
+    			break;
             
             case InputKeyBack:
                 confirm_removal = false;
@@ -43,29 +41,29 @@ static void confirm_input_callback(InputEvent* event, void* context) {
 static void confirm_render_callback(Canvas* canvas, void* context) {
     UNUSED(canvas);
     UNUSED(context);
-    canvas_draw_icon(canvas, 2, 55, &I_ButtonLeft_4x7)
-	canvas_draw_icon(canvas, 122, 55, &I_ButtonRight_4x7)
+    canvas_draw_icon(canvas, 2, 55, &I_ButtonLeft_4x7);
+	canvas_draw_icon(canvas, 122, 55, &I_ButtonRight_4x7);
 	canvas_draw_line(canvas, 1, 51, 128, 51);
 	canvas_set_font(canvas, FontPrimary);
-	canvas_draw_str(canvas, 9, 62, "No")
+	canvas_draw_str(canvas, 9, 62, "No");
 	canvas_set_font(canvas, FontPrimary);
-	canvas_draw_str(canvas, 99, 62, "Yes")
+	canvas_draw_str(canvas, 99, 62, "Yes");
 	canvas_set_font(canvas, FontPrimary);
-	canvas_draw_str(canvas, 53, 11, "Confirmation:")
-	canvas_draw_icon(canvas, 2, 7, &I_WarningDolphin_45x42)
+	canvas_draw_str(canvas, 53, 11, "Confirmation:");
+	canvas_draw_icon(canvas, 2, 7, &I_WarningDolphin_45x42);
 	canvas_set_font(canvas, FontSecondary);
-	canvas_draw_str(canvas, 55, 22, "Are you sure to")
+	canvas_draw_str(canvas, 55, 22, "Are you sure to");
 	canvas_set_font(canvas, FontSecondary);
-	canvas_draw_str(canvas, 50, 31, "delete the useless")
+	canvas_draw_str(canvas, 50, 31, "delete the useless");
 	canvas_set_font(canvas, FontSecondary);
-	canvas_draw_str(canvas, 51, 40, "hiddenfiles in your")
+	canvas_draw_str(canvas, 51, 40, "hiddenfiles in your");
 	canvas_set_font(canvas, FontSecondary);
-	canvas_draw_str(canvas, 48, 49, "external SD card?")
-	canvas_draw_icon(canvas, 85, 54, &I_SDcardMounted_11x8)
-	canvas_draw_icon(canvas, 2, 3, &I_unknown_10px)
-	canvas_draw_icon(canvas, 36, 54, &I_Pin_back_arrow_10x8)
+	canvas_draw_str(canvas, 48, 49, "external SD card?");
+	canvas_draw_icon(canvas, 85, 54, &I_SDcardMounted_11x8);
+	canvas_draw_icon(canvas, 2, 3, &I_unknown_10px);
+	canvas_draw_icon(canvas, 36, 54, &I_Pin_back_arrow_10x8);
 	canvas_set_font(canvas, FontPrimary);
-	canvas_draw_str(canvas, 49, 62, "Back")
+	canvas_draw_str(canvas, 49, 62, "Back");
 }
 
 void show_confirmation_screen() {
