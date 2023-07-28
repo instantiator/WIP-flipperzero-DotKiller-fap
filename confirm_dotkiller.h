11fbfd49c63ff93a4b755b6689783bbabdbdab44
47LeCoste <82815207+grugnoymeme@users.noreply.github.com>
2023-07-28 20:54:41 +0200

    Update and rename confirm_dotkiller.h to macos_dotkiller.h

--- confirm_dotkiller.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIRM_DOTKILLER_H
-#define CONFIRM_DOTKILLER_H
-
-#include <furi.h>
-
-void show_confirmation_screen();
-
-#endif // CONFIRM_DOTKILLER_H
---
+++ macos_dotkiller.h
@@ -0,0 +1,8 @@
+#ifndef MACOS_DOTKILLER_H
+#define MACOS_DOTKILLER_H
+
+#include <furi.h>
+
+void show_confirmation_screen();
+
+#endif // MACOS_DOTKILLER_H
